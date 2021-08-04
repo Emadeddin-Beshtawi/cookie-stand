11deed58fb1357454e2d0a815e51458bd6ee5516
Emadeddin-Beshtawi <engemadeddin@gmail.com>
2021-08-04 13:07:47 +0300

    css again

--- js/app.js
+++ js/app.js
@@ -1,32 +1,45 @@
 'use strict';
-let hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','total'];
+let hours=['6 AM','7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','total'];
 let tableID=document.getElementById('tableID');
 let arrTotal=[0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 let totals=0;
 
-//random value generator
+
+
 function randomValue(min,max){
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+
+
 function Store(location,minCust,maxCust,avgCook){
   this.location=location;
+
   this.minCust=minCust;
+
   this.maxCust=maxCust;
+
   this.avgCook=avgCook;
+
   this.custPerHour=[];
+
   this.cook=[];
+  
   this.total=0;
 }
 
-//cust per hour
+
+
+
+
 Store.prototype.calccustPerHour=function(){
   for(let i=0;i<hours.length-1;i++){
     this.custPerHour.push(randomValue(this.minCust,this.maxCust));
   }
 };
 
-//calcuate the cookie per hour
+
+
 Store.prototype.calccookPerhour=function(){
   for(let i=0;i<hours.length-1;i++){
     this.cook.push(this.custPerHour[i]*Math.floor(this.avgCook));
@@ -37,7 +50,8 @@ Store.prototype.calccookPerhour=function(){
   totals+=this.total;
 };
 
-//build table
+
+
 Store.prototype.createElement=function(){
   let tableRow=document.createElement('tr');
   tableID.appendChild(tableRow);
@@ -52,72 +66,83 @@ Store.prototype.createElement=function(){
   }
 };
 
-//creat heading
-function heading(){
-  let thead=document.createElement('thead');
+
+
+
+function heading()
+{
+  let thead = document.createElement('thead');
   tableID.appendChild(thead);
+
   let tableRow=document.createElement('tr');
   thead.appendChild(tableRow);
-  let tdEl1=document.createElement('td');
+  let tdEl1 = document.createElement('td');
   tableRow.appendChild(tdEl1);
   tdEl1.textContent='stores';
   let thEl;
-  for (let i=0;i<hours.length;i++){
+
+  for (let i = 0;i<hours.length;i++){
     thEl=document.createElement('th');
     tableRow.appendChild(thEl);
     thEl.textContent=hours[i];
   }
 }
 
-//creat footer
+
+
+
 function footers(){
-  let footer=document.createElement('tfoot');
+  let footer = document.createElement('tfoot');
   tableID.appendChild(footer);
-  let tableRow=document.createElement('tr');
+
+  let tableRow = document.createElement('tr');
   footer.appendChild(tableRow);
-  let tdEl1=document.createElement('td');
+
+  let tdEl1 = document.createElement('td');
   tableRow.appendChild(tdEl1);
-  tdEl1.textContent='total';
+  tdEl1.textContent = 'total';
   let tdEl;
-  for (let i=0;i<hours.length-1;i++){
+
+  for (let i=0; i<hours.length-1; i++){
     tdEl=document.createElement('td');
     tableRow.appendChild(tdEl);
-    tdEl.textContent=`${arrTotal[i]}`;
+    tdEl.textContent = `${arrTotal[i]}`;
   }
-  tdEl=document.createElement('td');
+
+  tdEl = document.createElement('td');
   tableRow.appendChild(tdEl);
   tdEl.textContent=`${totals}`;
 }
 
 heading();
 
-//call the elements of objects to the table
 
-let seattle=new Store('seattle',23,65,6.3);
+
+let seattle = new Store('Seattle',23,65,6.3);
 seattle.calccustPerHour();
 seattle.calccookPerhour();
 seattle.createElement();
 
 
-let tokyo=new Store('tokyo',3,24,1.2);
+let tokyo = new Store('Tokyo',3,24,1.2);
 tokyo.calccustPerHour();
 tokyo.calccookPerhour();
 tokyo.createElement();
 
 
-let dubai=new Store('Dubai',11,38,3.7);
+let dubai = new Store('Dubai',11,38,3.7);
 dubai.calccustPerHour();
 dubai.calccookPerhour();
 dubai.createElement();
 
 
-let paris=new Store('Paris',20,38,2.3);
+let paris = new Store('Paris',20,38,2.3);
 paris.calccustPerHour();
 paris.calccookPerhour();
 paris.createElement();
 
 
-let lima=new Store('lima',2,16,4.6);
+let lima = new Store('Lima',2,16,4.6);
 lima.calccustPerHour();
 lima.calccookPerhour();
 lima.createElement();
